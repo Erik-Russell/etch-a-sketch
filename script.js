366d04b9ea893c4d3fb5de7dc237220e9dfae980
Erik Russell <erik.z.russell@gmail.com>
2024-08-13 07:42:09 -0700

    added button 'new'. Button will remove current grid, prompt user for size of new grid, and draw new grid

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 const grid = document.querySelector('.grid-container');
 
+let squaresPerSide = 4;
+let gridSize = squaresPerSide ** 2;
+let gridLength = 100 / Math.floor(Math.sqrt(gridSize));
+let width = `1 1 ${gridLength}vw`;
+let height = `${gridLength}vw`;
 
-for (let index = 0; index < 16; index++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
+let newButton = document.querySelector('.new');
+newButton.addEventListener('click', () => {
+    squaresPerSide = prompt("Enter number of squares per side", "4");
+    gridSize = squaresPerSide ** 2;
+    clearGrid();
+    drawGrid(gridSize);
+
+});
+
+function drawGrid(numberOfSquares) {
+    for (let index = 0; index < numberOfSquares; index++) {
+        gridLength = 100 / Math.floor(Math.sqrt(numberOfSquares));
+        width = `1 1 ${gridLength}vw`;
+        height = `${gridLength}vw`;
+
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.flex = width;
+        square.style.height = height;
+        grid.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'green';
+        });
+    };
+    console.log('drawGrid');
 }
 
-let squares = document.querySelectorAll('.square');
-for (const square of squares) {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'green';
-    });
+function clearGrid() {
+    let squares = document.querySelectorAll('.square');
+    for (const square of squares) {
+        grid.removeChild(square);
+    };
+    console.log('clearGrid');
 }
+
+
+drawGrid(gridSize);
