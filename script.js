17b5f5dc1573aebc3529e9df6523d12e034c3bea
Erik Russell <erik.z.russell@gmail.com>
2024-08-13 06:09:36 -0700

    js function for hover => change background of div

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const grid = document.querySelector('.grid-container');
 for (let index = 0; index < 16; index++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.textContent = 'testing';
     grid.appendChild(square);
+}
+
+let squares = document.querySelectorAll('.square');
+for (const square of squares) {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'green';
+    });
 }
